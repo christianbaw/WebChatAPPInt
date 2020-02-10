b52b75568156cb905981ac593bcb4d5746d6dcce
Christian Bawmann <Christiangbm@hotmail.com>
2020-02-10 15:25:51 -0500

    First Commit

---
+++ WebChatAPP.Data/Models/AppUser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace WebChatAPP.Data.Models
+{
+    public class AppUser
+    {
+        
+    }
+}
---
+++ WebChatAPP.Data/Models/Message.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace WebChatAPP.Data.Models
+{
+    public class Message
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string Text { get; set; }
+        public DateTime MessageDate { get; set; }
+    }
+}
