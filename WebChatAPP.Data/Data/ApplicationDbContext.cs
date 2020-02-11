711c86df352d8d6b1d02c6e584506b3c4cdd9d48
Christian Bawmann <Christiangbm@hotmail.com>
2020-02-11 10:16:03 -0500

    moved out application db context from the main project to another layer

--- WebChatAPP.Data/Data/ApplicationDbContext.cs
+++ WebChatAPP.Data/Data/ApplicationDbContext.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using WebChatAPP.Data.Interfaces;
+using WebChatAPP.Data.Models;
 
 namespace WebChatAPP.Data
 {
@@ -11,6 +13,22 @@ namespace WebChatAPP.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+
+
         }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<Message>()
+                .HasOne<AppUser>(a => a.Sender)
+                .WithMany(d => d.Messages)
+                .HasForeignKey(e => e.UserID);
+        }
+
+       
+        public DbSet<Message> Messages { get; set; }
+        
     }
+    
 }
