e4f5b3a5c2972fe108134797646e24a9076b7a6a
Christian Bawmann <Christiangbm@hotmail.com>
2020-02-11 10:14:52 -0500

    added signalR hub to the project

---
+++ WebChatAPP/Hubs/ChatHub.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+using WebChatAPP.Data.Models;
+
+namespace WebChatAPP.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(Message message)
+        {
+            if (message.Text.Contains("stock"))
+
+            {
+                message.Text = message.Text + "testing";
+                await Clients.All.SendAsync("receiveMessage", new { message.UserName, message.Text, message.MessageDate });
+            }
+            else
+            {
+                await Clients.All.SendAsync("receiveMessage", new { message.UserName, message.Text, message.MessageDate });
+            }
+            
+        }
+    }
+}
