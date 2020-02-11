d169490c08129b5e8900181def0d842cc2f787d6
Christian Bawmann <Christiangbm@hotmail.com>
2020-02-11 10:14:23 -0500

    scaffolding of identity pages

--- WebChatAPP/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ WebChatAPP/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -11,20 +11,18 @@ using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using WebChatAPP.Data.Models;
 
 namespace WebChatAPP.Areas.Identity.Pages.Account
 {
     [AllowAnonymous]
     public class LoginModel : PageModel
     {
-        private readonly UserManager<IdentityUser> _userManager;
-        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
         private readonly ILogger<LoginModel> _logger;
 
-        public LoginModel(SignInManager<IdentityUser> signInManager, 
-            ILogger<LoginModel> logger,
-            UserManager<IdentityUser> userManager)
-        {
+        public LoginModel(SignInManager<AppUser> signInManager, ILogger<LoginModel> logger, UserManager<AppUser> userManager)   {
             _userManager = userManager;
             _signInManager = signInManager;
             _logger = logger;
