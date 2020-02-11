22b439cd9e7feb98b14890811b48249c1ead6f96
Christian Bawmann <Christiangbm@hotmail.com>
2020-02-11 10:14:37 -0500

    modified home controller and added messagecontroller

--- WebChatAPP/Controllers/HomeController.cs
+++ WebChatAPP/Controllers/HomeController.cs
@@ -1,38 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using WebChatAPP.Data.Models;
-
+using WebChatAPP.Data.Interfaces;
 
 namespace WebChatAPP.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IChatRepository _repository;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IChatRepository repository)
         {
             _logger = logger;
+            _repository = repository;
         }
-
-        public IActionResult Index()
+      
+        public async Task<IActionResult> Index()
         {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            var currentLoggedUser = await _repository.currentUser();
+            ViewBag.CurrentUserName = currentLoggedUser;
+            var getMessages = _repository.getMessages();
+            return View(getMessages);
         }
     }
 }
---
+++ WebChatAPP/Controllers/MessageController.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebChatAPP.Data.Interfaces;
+using WebChatAPP.Data.Models;
+
+namespace WebChatAPP.Controllers
+{
+    [Route("api/[controller]")]
+    public class MessageController : ControllerBase
+    {
+        private readonly IChatRepository _repository;
+        
+        public MessageController(IChatRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddMessage(MessageVM messageVM)
+        {
+            var message = new Message
+            {
+                UserID = await _repository.getUserID(),
+                UserName = User.Identity.Name,
+                Text = messageVM.Text
+            };
+
+            _repository.insertrecord(message);
+            return Ok(new { message.UserName, message.Text, message.MessageDate });
+        }
+    }
+}
