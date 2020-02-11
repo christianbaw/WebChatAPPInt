1f8867368be7597e42f37123aaf8bba7a097403c
Christian Bawmann <Christiangbm@hotmail.com>
2020-02-11 10:16:19 -0500

    added handler and chat js

---
+++ WebChatAPP/wwwroot/js/chat.js
@@ -0,0 +1,23 @@
+﻿const username = userName;
+const textInput = $('#messageText');
+
+document.getElementById('submitButton').addEventListener('click', () => {
+    let text = textInput.val();
+    $.post("api/message", { text }).done(saveMessageResponse);
+});
+
+function saveMessageResponse(response) {
+    textInput.val('');
+    sendMessageToHub(response);
+}
+
+function addMessageToChat(message) {
+    let container = $('<div class="row REPLACEME">');
+    let usermessage = (username == message.userName) ? '<text> justify-content-end </text>' : '<text> justify-content-start </text>';
+    let template = '<div class="col-lg-3">CONTENT</div>';
+    container.append($(usermessage.replace('REPLACEME', usermessage)));
+    container.append($(template.replace('CONTENT', message.userName)));
+    container.append($(template.replace('CONTENT', message.text)));
+    //container.append($(template.replace('CONTENT', message.messageDate)));
+    $('#chat').append(container);
+}
