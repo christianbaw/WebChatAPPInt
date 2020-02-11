7ead700921c66132a84e008c46fec4e571261616
Christian Bawmann <Christiangbm@hotmail.com>
2020-02-11 10:15:48 -0500

    added chat repository interface

---
+++ WebChatAPP.Data/Interfaces/IChatRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using WebChatAPP.Data.Models;
+
+namespace WebChatAPP.Data.Interfaces
+{
+    public interface IChatRepository
+    {
+        Task<string> currentUser();
+
+        List<Message> getMessages();
+
+        Task<string> getUserID();
+
+        void insertrecord(Message message);
+    }
+}
